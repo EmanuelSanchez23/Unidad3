ed7e01e7c96695597d2134024769282e962c4f01
Emanuel <143622434+EmanuelSanchez23@users.noreply.github.com>
2023-09-26 18:53:01 -0600

    Add files via upload

---
+++ Practica1u3.java
@@ -0,0 +1,21 @@
+
+package practica1u3;
+import java.util.Scanner;
+
+public class Practica1u3 {
+    public static void main(String[] args) {
+       
+        Scanner leer=  new Scanner(System.in);
+        System.out.println("Ingtresa numero");
+        int num= leer.nextInt();
+        if (num%2==0) {
+            System.out.println("Es par");
+            
+        }else{
+            System.out.println("No es par");
+        }
+                 }   
+        }
+    
+    
+
